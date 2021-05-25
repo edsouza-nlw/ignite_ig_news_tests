b50177b566f4edce15e09e984e03c380dbcdc76a
edrajao <4ucode@gmail.com>
2021-05-25 07:50:13 -0300

    Stop Install StripeWindows

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Ed Rajao
+      {session.user.name}
       <FiX className={styles.closeIcon} color="#737380" />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
